4cd46b448ef1583f62999e8818016d01dfdb60b0
rooparsh <rooparshkalia@gmail.com>
2022-10-05 19:29:17 +0530

    day 3 leetcode_75 done

---
+++ src/main/kotlin/leetcode_75/level_1/day_3/LinkedListCycle.kt
@@ -0,0 +1,52 @@
+package leetcode_75.level_1.day_3
+
+import two_weeks_study_plan.algorithm_1.day_5.ListNode
+
+/**
+ * Difficulty : Easy
+ * Problem Statement : https://leetcode.com/problems/linked-list-cycle-ii/?envType=study-plan&id=level-1
+ */
+fun main() {
+
+    val listNode4 = ListNode(-4)
+    val listNode0 = ListNode(0).apply { next = listNode4 }
+    val listNode2 = ListNode(2).apply { next = listNode0 }
+    val listNode3 = ListNode(4).apply { next = listNode2 }
+
+
+    listNode4.next = listNode2
+
+    println(LinkedListCycle().detectCycle(listNode3)?.`val`)
+
+}
+
+class LinkedListCycle {
+    fun detectCycle(head: ListNode?): ListNode? {
+        if (head?.next == null) return null
+
+        var turtle = head
+        var hare = head
+
+        while (true) {
+            if (hare?.next == null) {
+                return null
+            }
+
+            turtle = turtle?.next
+            hare = hare.next?.next
+
+            if (turtle == hare) {
+                break
+            }
+        }
+
+        var p1 = head
+        var p2 = hare
+        while (p1 != p2) {
+            p1 = p1?.next
+            p2 = p2?.next
+        }
+
+        return p1
+    }
+}
---
+++ src/main/kotlin/leetcode_75/level_1/day_3/MergeSortedList.kt
@@ -0,0 +1,44 @@
+package leetcode_75.level_1.day_3
+
+import two_weeks_study_plan.algorithm_1.day_5.ListNode
+
+/**
+ * Difficulty : Medium
+ * Problem Statement : https://leetcode.com/problems/squares-of-a-sorted-array/?envType=study-plan&id=algorithm-i
+ */
+
+fun main() {
+
+    val listNode4 = ListNode(4)
+    val listNode2 = ListNode(2).apply { next = listNode4 }
+    val listNode1 = ListNode(1).apply { next = listNode2 }
+
+
+    val listNode4New = ListNode(4)
+    val listNode3New = ListNode(2).apply { next = listNode4New }
+    val listNode1New = ListNode(1).apply { next = listNode3New }
+
+    println(MergeSortedList().mergeTwoLists(listNode1, listNode1New)?.`val`)
+
+}
+
+class MergeSortedList {
+    fun mergeTwoLists(list1: ListNode?, list2: ListNode?): ListNode? {
+
+        if (list1 == null) return list2
+        if (list2 == null) return list1
+
+        val linkedList = ListNode(0)
+
+        if (list1.`val` < list2.`val`) {
+            linkedList.`val` = list1.`val`
+            linkedList.next = mergeTwoLists(list1.next, list2)
+        } else {
+            linkedList.`val` = list2.`val`
+            linkedList.next = mergeTwoLists(list1, list2.next)
+        }
+
+
+        return linkedList
+    }
+}
