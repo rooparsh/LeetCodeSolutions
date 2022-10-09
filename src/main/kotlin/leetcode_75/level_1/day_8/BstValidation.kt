fc93bb8610b2bd36e4fe079ee297c493aecd1df6
rooparsh <rooparshkalia@gmail.com>
2022-10-09 18:44:20 +0530

    BST validation

---
+++ src/main/kotlin/leetcode_75/level_1/day_8/BstValidation.kt
@@ -0,0 +1,22 @@
+package leetcode_75.level_1.day_8
+
+import leetcode_75.level_1.day_6.TreeNode
+
+/**
+ * Difficulty : Medium
+ * Problem Statement : https://leetcode.com/problems/validate-binary-search-tree/?envType=study-plan&id=level-1
+ */
+fun main() {
+
+}
+
+class BstValidation {
+    fun isValidBST(root: TreeNode?, minValue: Long = Long.MIN_VALUE, maxValue: Long = Long.MAX_VALUE): Boolean {
+        root ?: return true
+        if (root.`val` <= minValue || root.`val` >= maxValue) return false
+
+        val leftValid = isValidBST(root.left, minValue, root.`val`.toLong())
+        val rightValid = isValidBST(root.right, root.`val`.toLong(), maxValue)
+        return leftValid && rightValid
+    }
+}
