e76a93401e7f7f78ac128b7cb3897de70dd09c3c
rooparsh <rooparshkalia@gmail.com>
2022-10-09 19:00:34 +0530

    lowest common ancestor BST

---
+++ src/main/kotlin/leetcode_75/level_1/day_8/LowestCommonAncestorNodeInBst.kt
@@ -0,0 +1,20 @@
+package leetcode_75.level_1.day_8
+
+import leetcode_75.level_1.day_6.TreeNode
+
+/**
+ * Difficulty : Medium
+ * Problem Statement : https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-search-tree/?envType=study-plan&id=level-1
+ */
+class LowestCommonAncestorNodeInBst {
+
+
+    fun lowestCommonAncestor(root: TreeNode?, p: TreeNode?, q: TreeNode?): TreeNode? {
+
+        if (root == null || p == null || q == null) return null
+
+        return if (p.`val` < root.`val` && q.`val` < root.`val`) lowestCommonAncestor(root.left, p, q)
+        else if (p.`val` > root.`val` && q.`val` > root.`val`) lowestCommonAncestor(root.right, p, q)
+        else root
+    }
+}
