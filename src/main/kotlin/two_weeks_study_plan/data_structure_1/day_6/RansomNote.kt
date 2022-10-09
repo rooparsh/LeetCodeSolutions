a98218e568016eb777a7c6d4780f8cb08e1e6a67
rooparsh <rooparshkalia@gmail.com>
2022-10-09 15:05:54 +0530

    day 6 - ransom note

---
+++ src/main/kotlin/two_weeks_study_plan/data_structure_1/day_6/RansomNote.kt
@@ -0,0 +1,24 @@
+package two_weeks_study_plan.data_structure_1.day_6
+
+/**
+ * Difficulty : Easy
+ * Problem Statement : https://leetcode.com/problems/ransom-note/?envType=study-plan&id=data-structure-i
+ */
+
+fun main() {
+    println(RansomNote().canConstruct("aa", "ab"))
+}
+
+class RansomNote {
+    fun canConstruct(ransomNote: String, magazine: String): Boolean {
+        val magazineMap = magazine.groupingBy { it }.eachCount()
+        val ransomMap = ransomNote.groupingBy { it }.eachCount()
+
+        ransomMap.forEach { (key, value) ->
+            if (magazineMap.containsKey(key).not() || magazineMap[key]!! < value) {
+                return false
+            }
+        }
+        return true
+    }
+}
