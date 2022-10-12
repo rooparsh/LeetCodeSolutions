4e33a42a933524180e9ec49897e3d5e539630b97
rooparsh <rooparshkalia@gmail.com>
2022-10-12 21:00:30 +0530

    largest perimeter triangle

---
+++ src/main/kotlin/random/LargestPerimeterTriangle.kt
@@ -0,0 +1,24 @@
+package random
+
+/**
+ * Difficulty : Easy
+ * Problem Statement : https://leetcode.com/problems/largest-perimeter-triangle/
+ */
+
+fun main() {
+    println(LargestPerimeterTriangle().largestPerimeter(intArrayOf(1, 2, 1)))
+}
+
+class LargestPerimeterTriangle {
+    fun largestPerimeter(nums: IntArray): Int {
+        val sortedArray = nums.sortedArray()
+
+        for (i in nums.lastIndex downTo 2) {
+            val longestSide = sortedArray[i]
+            val perimeter = longestSide + sortedArray[i - 1] + sortedArray[i - 2]
+            val sumOfRemainSides = sortedArray[i - 1] + sortedArray[i - 2]
+            if (sumOfRemainSides > longestSide) return perimeter
+        }
+        return 0
+    }
+}
