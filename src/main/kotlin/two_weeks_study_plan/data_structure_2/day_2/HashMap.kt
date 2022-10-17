39b58af41bb765afcf291fb378587af08ba8cdbd
rooparsh <rooparshkalia@gmail.com>
2022-10-17 20:50:00 +0530

    hashmap

---
+++ src/main/kotlin/two_weeks_study_plan/data_structure_2/day_2/HashMap.kt
@@ -0,0 +1,23 @@
+package two_weeks_study_plan.data_structure_2.day_2
+
+/**
+ * Difficulty : Easy
+ * Problem Statement : https://leetcode.com/problems/design-hashmap/?envType=study-plan&id=data-structure-ii
+ */
+
+class HashMap {
+
+    private val array = arrayOfNulls<Int>(1000001)
+
+    fun put(key: Int, value: Int) {
+        array[key] = value
+    }
+
+    fun get(key: Int): Int {
+        return array[key] ?: -1
+    }
+
+    fun remove(key: Int) {
+        array[key] = null
+    }
+}
