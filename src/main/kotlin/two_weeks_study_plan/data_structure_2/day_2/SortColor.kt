e8e8301c74e64544265a1eb12e5d6fa7b9ff3ba8
rooparsh <rooparshkalia@gmail.com>
2022-10-17 20:31:54 +0530

    sort colors

---
+++ src/main/kotlin/two_weeks_study_plan/data_structure_2/day_2/SortColor.kt
@@ -0,0 +1,31 @@
+package two_weeks_study_plan.data_structure_2.day_2
+
+/**
+ * Difficulty : Medium
+ * Problem Statement : https://leetcode.com/problems/sort-colors/?envType=study-plan&id=data-structure-ii
+ */
+
+fun main() {
+    val array = intArrayOf(2, 0, 2, 1, 1, 0)
+    SortColor().sortColors(array)
+    print(array.joinToString())
+}
+
+class SortColor {
+    fun sortColors(nums: IntArray) {
+        var startId = 0
+        while (startId < nums.size - 1) {
+            var smallestIdx = startId
+            for (i in startId + 1 until nums.size) {
+                if (nums[smallestIdx] > nums[i]) {
+                    smallestIdx = i
+                }
+            }
+            val temp = nums[startId]
+            nums[startId] = nums[smallestIdx]
+            nums[smallestIdx] = temp
+            startId++
+
+        }
+    }
+}
