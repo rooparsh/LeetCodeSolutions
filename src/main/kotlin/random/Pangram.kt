ba132e17828238db3b8cb03441ef6afbe60459b5
rooparsh <rooparshkalia@gmail.com>
2022-10-17 20:37:41 +0530

    pangram

---
+++ src/main/kotlin/random/Pangram.kt
@@ -0,0 +1,16 @@
+package random
+
+/**
+ * Difficulty : Easy
+ * Problem Statement : https://leetcode.com/problems/check-if-the-sentence-is-pangram/
+ */
+
+fun main() {
+    print(Pangram().checkIfPangram("thequickbrownfoxjumpsoverthelazydog"))
+}
+
+class Pangram {
+    fun checkIfPangram(sentence: String): Boolean {
+        return sentence.toList().distinct().size == 26
+    }
+}
