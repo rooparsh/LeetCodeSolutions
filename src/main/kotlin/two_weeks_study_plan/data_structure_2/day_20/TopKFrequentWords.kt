8f6e0bba8906eee8487f6b26d14be878cb87a719
rooparsh <rooparshkalia@gmail.com>
2022-11-04 08:56:14 +0530

    top k frequent words

---
+++ src/main/kotlin/two_weeks_study_plan/data_structure_2/day_20/TopKFrequentWords.kt
@@ -0,0 +1,21 @@
+package two_weeks_study_plan.data_structure_2.day_20
+
+/**
+ * Difficulty : Medium
+ * Problem Statement : https://leetcode.com/problems/top-k-frequent-elements/?envType=study-plan&id=data-structure-ii
+ */
+fun main() {
+    println(TopKFrequentWords().topKFrequent(intArrayOf(4, 1, -1, 2, -1, 2, 3), 2).joinToString())
+}
+
+class TopKFrequentWords {
+    fun topKFrequent(nums: IntArray, k: Int): IntArray {
+        val frequencyMap = mutableMapOf<Int, Int>()
+
+        nums.forEach {
+            frequencyMap[it] = frequencyMap[it]?.plus(1) ?: 1
+        }
+
+        return frequencyMap.toList().sortedByDescending { it.second }.map { it.first }.subList(0, k).toIntArray()
+    }
+}
